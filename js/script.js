9ec8cbf67a7109046310cbe07ed2784f85b2c8c5
Gladys-nimo <wkanyora10@gmail.com>
2020-10-08 18:24:07 +0300

    finally my code did work

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ function myFunction() {
     var myStringDate = document.getElementById("dateProvided").Value;
      var days = ["Sunday", "Monday", "Wednesday", "Thursday", "Friday", "Saturday"];
      var maleAkan = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-     var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua","Ama"]
-    var myDate = new Date(myStringDate);
-   var dayOfWeek = myDate.getDay();
+     var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua","Ama"];
+    var g = new Date(myStringDate);
+   var dayOfWeek = g.getDay();
    var gender = document.querySelector('input[name="gender"]:checked').value;
-   document.getElementById("message").innerHTML = "I was born on " + days[dayOfWeek] + "!";
-  document.getElementById("message").innerHTML = "my maleAkan name is " + maleAkan[dayOfWeek] + "!";
-  document.getElementById("message").innerHTML = "my femaleAkan name is " + femaleAkan[dayOfWeek] + "!";
+   document.getElementById("message").innerHTML = "I was born on " + days['0', '1', '2', '3', '4', '5', '6']+ "!";
+  document.getElementById("message").innerHTML = "my maleAkan name is " + maleAkan['0', '1', '2','3','4', '5', '6'] + "!";
+  document.getElementById("message").innerHTML = "my femaleAkan name is " + femaleAkan['0', '1', '2', '3', '4', '5', '6'] + "!";
 }
   
 
