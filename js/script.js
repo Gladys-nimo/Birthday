436de126655825385d8470af00538ce020b31aa3
Gladys-nimo <wkanyora10@gmail.com>
2020-10-19 00:33:53 +0300

    added some js files

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
-function myFunction() {
-    var myStringDate = document.getElementById("dateProvided").Value;
-     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-     var maleAkan = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-     var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua","Ama"];
-    var g = new Date(myStringDate);
-   var dayOfWeek = g.getDay();
-   var gender = document.querySelector('input[name="gender"]:checked').value;
-   document.getElementById("message").innerHTML = "I was born on " + days['0', '1', '2', '3', '4', '5', '6'] + "!";
-  document.getElementById("message").innerHTML = "my maleAkan name is " + maleAkan['0', '1', '2','3','4', '5', '6'] + "!";
-  document.getElementById("message").innerHTML = "my femaleAkan name is " + femaleAkan['0', '1', '2', '3', '4', '5', '6'] + "!";
-}
+let doc = document.getElementById("form").addEventListener("submit",myFunction)
+console.log(doc)
+
+
+function myFunction(event) {
+  event.preventDefault();
+  var dayofBirth = document.getElementById("birthday").value.split("-");
+  var gender = document.getElementById("gender").value;
+  console.log(dayofBirth);
+  if (dayofBirth != "") {
+      var MM = parseInt(dayofBirth[1]);
+      var DD = parseInt(dayofBirth[2]);
+      var Year = dayofBirth[0].split("");
+      var CC = parseInt(Year[0].concat(Year[1]));
+      var YY = parseInt(Year[2].concat(Year[3]));
+      var d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+      var d1 = Math.floor(d);
+      var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kwame"];
+      var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+      if (gender === "male" && d1 >= 0 && d1 <= 6) {
+          alert("Your Akan name is " + akanMale[d1] + "and you were born on" + [d1]);
+      }
+      else if (gender === "female" && d1 >= 0 && d1 <= 6) {
+          alert("Your Akan name is " + akanFemale[d1])
+      }
+      else {
+          alert("invalid input");
+      }
+  }
+  else {
+      alert("invalid input");
+  }
+  document.getElementById("form").reset();
+   Selector('input[name="gender"]:checked').value;
+
   
-
-
-
-
-   
+}
